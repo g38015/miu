3946ac22da9c312e14db10b65e95e2a7a9646766
appstrategy <eprleads@gmail.com>
2013-06-03 19:55:38 -0700

    initial project structure for MIU from week 4 VFW

---
+++ week1/js/json.js
@@ -0,0 +1,39 @@
+// JSON Lead Data to Populate
+
+var json = {
+
+    "lead1": {
+        "name": ["Name:", "Peter"],
+        "phone": ["Phone:", "415-555-1212"],
+        "email": ["Email:", "peter@test.com"],
+        "date": ["Date:", "2013-05-15"],
+        "check": ["Checked:", "townhome"],
+        "price": ["Price:", "250000"],
+        "bedrooms": ["Bedrooms:", "3+"],
+        "info": ["Info:", "Test"]
+    },
+    
+    "lead2": {
+        "name": ["Name:", "Ellie"],
+        "phone": ["Phone:", "415-555-1212"],
+        "email": ["Email:", "ellie@test.com"],
+        "date": ["Date:", "2013-05-15"],
+        "check": ["Checked:", "Sfr"],
+        "price": ["Price:", "300000"],
+        "bedrooms": ["Bedrooms:", "1+"],
+        "info": ["Info:", "New One"]
+    },
+    
+    "lead3": {
+        "name": ["Name:", "Ruby"],
+        "phone": ["Phone:", "415-555-1212"],
+        "email": ["Email:", "ruby@test.com"],
+        "date": ["Date:", "2013-06-18"],
+        "check": ["Checked:", "condo"],
+        "price": ["Price:", "350000"],
+        "bedrooms": ["Bedrooms:", "2+"],
+        "info": ["Info:", "Test"]
+    }
+    
+
+}
