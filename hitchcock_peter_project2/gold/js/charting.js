7dd7be5e5f53c7f12a6d0ab2918e65713adf1f26
appstrategy <eprleads@gmail.com>
2013-06-13 11:26:30 -0700

    gold app added about page, moved chart.js into header of index, updated main feature, updated images to create similar look and feel across the app, gold app COMPLETE Whooo Hooo

--- hitchcock_peter_project2/gold/js/charting.js
+++ hitchcock_peter_project2/gold/js/charting.js
@@ -1,29 +1,20 @@
-var lineChart = {
-			labels : [" "," "," "," "," "," "," "],
-			datasets : [
+var pieData = [
 				{
-					fillColor : "rgba(220,220,220,0.5)",
-					strokeColor : "rgba(220,220,220,1)",
-					pointColor : "rgba(220,220,220,1)",
-					pointStrokeColor : "#fff",
-					data : [65,59,90,81,56,55,40]
+					value: 30,
+					color:"#F38630"
 				},
 				{
-					fillColor : "rgba(151,187,205,0.5)",
-					strokeColor : "rgba(151,187,205,1)",
-					pointColor : "rgba(151,187,205,1)",
-					pointStrokeColor : "#fff",
-					data : [28,48,40,19,96,27,100]
+					value : 50,
+					color : "#E0E4CC"
+				},
+				{
+					value : 100,
+					color : "#69D2E7"
 				}
-			]
 
-		}
+			];
 
-var newLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChart);
-
-
-
-
+	var myPie = new Chart(document.getElementById("canvas").getContext("2d")).Pie(pieData);
 
 
 
