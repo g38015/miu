5697f2a18e15fc5cc29b83e55eff1ff6879be026
appstrategy <eprleads@gmail.com>
2013-06-18 10:49:46 -0700

    created good defaults for gold and bronze, prominent done button placed, same page erros in input fields gold at top of form bronze, text clear button integrated in some fields for gold

--- hitchcock_peter_project3/gold/js/main.js
+++ hitchcock_peter_project3/gold/js/main.js
@@ -1,18 +1,17 @@
-var storeData = function(data) {
-    // uses form data
-    console.log(data);
-
+// getElementsById Function
+function xyz(x) {
+    var getElement = document.getElementById(x);
+    return getElement;
 };
-/*
+
 $('#home').on('pageinit', function(){
 	//code needed for home page goes here
 	
 });	
-*/
 
 		
 $('#additem').on('pageinit', function(){
-
+    
 		var myForm = $('#contactForm'),
 		    errorlink = $('#errorslink')
 		
@@ -20,18 +19,19 @@ $('#additem').on('pageinit', function(){
 		    myForm.validate({
 			invalidHandler: function(form, validator) {
 			    errorlink.click();
-			    for (var key in validator.submitted) {
-			        
-			    }
+			   
 			},
 			submitHandler: function() {
 		var data = myForm.serializeArray();
 			storeData(data);
+			console.log(data);
 		}
+		
 	});
 	
 	//any other code needed for addItem page goes here
 	
+		
 });
 
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
@@ -40,12 +40,37 @@ var autofillData = function (){
 	 
 };
 
-var getData = function(){
 
-};
+var storeData = function(key){
 
-var storeData = function(data){
-	
+    // if there is no key means brand new lead and need a new key
+    if(!key) {
+    
+        var id             = Math.floor(Math.random()*10000001);
+        
+    }else{
+        // set the id to the existing key we are editing
+            id = key;
+        }
+        // Get all form field values and store in object
+        // Object properties contain array form label and input value
+        // getSelectedRadio();
+        var lead           = {};
+            lead.name      = ["Name:", xyz("name").value];
+            lead.phone     = ["Phone:", xyz("phone").value];
+            lead.email     = ["Email:", xyz("email").value];
+            lead.date      = ["Date:", xyz("date").value];
+            // lead.type      = ["Checked:", typeValue];
+            lead.price     = ["Price:", xyz("price").value];
+            // lead.bedrooms  = ["Bedrooms:", xyz("bedrooms").value];
+            lead.info      = ["Info:", xyz("additional").value];
+            // lead.hidden    = ["Hidden:", xyz("hideme").value];
+            
+            
+        // Save data to local storage Use stringify to convert object to a string
+        localStorage.setItem(id, JSON.stringify(lead));
+        alert("Lead Has Been Saved!");
+                     
 }; 
 
 var	deleteItem = function (){
@@ -55,5 +80,7 @@ var	deleteItem = function (){
 var clearLocal = function(){
 
 };
+
+
 
 
